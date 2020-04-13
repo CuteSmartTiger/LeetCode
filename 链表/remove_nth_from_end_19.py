411c45dca92c91b47fee723a40abadb0bb4d69d1
invictus090 <ecustbaihu@163.com>
2020-04-13 22:41:04 +0800

    remove nth from end 19

---
+++ 链表/remove_nth_from_end_19.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/13 22:38
+# @Author  : liuhu
+# @Site    : 
+# @File    : remove_nth_from_end_19.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def remove_nth_from_end(head, n):
+    # 对空的判断
+    if not head:
+        return head
+    pre_head = ListNode(0)
+    pre_head.next = head
+    left = right = pre_head
+
+    # 相距n
+    while n:
+        right = right.next
+        n -= 1
+
+    while right and right.next:
+        left = left.next
+        right = right.next
+
+    left.next = left.next.next
+    return pre_head.next
