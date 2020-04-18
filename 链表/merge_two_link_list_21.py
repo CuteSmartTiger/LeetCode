730b7b5b443c74b8c914cd4b5a505f63ee398d10
invictus090 <ecustbaihu@163.com>
2020-04-18 22:49:56 +0800

    merge_two_link_list

---
+++ 链表/merge_two_link_list_21.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/18 22:47
+# @Author  : liuhu
+# @Site    : 
+# @File    : merge_two_link_list_21.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def merge_two_lists(l1, l2):
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    pre_head = cur = ListNode(-1)
+    while l1 and l2:
+        if l1.val <= l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+
+    if l1:
+        cur.next = l1
+
+    if l2:
+        cur.next = l2
+
+    # cur.next = l1 if l1 is not None else l2
+
+    return pre_head.next
