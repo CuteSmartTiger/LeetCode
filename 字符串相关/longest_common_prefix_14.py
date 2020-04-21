777715f77de86bf46f68ccb9d7efe728e47bd377
invictus090 <ecustbaihu@163.com>
2020-04-21 22:09:38 +0800

    longest_common_prefix

--- 字符串相关/longest_common_prefix_14.py
+++ 字符串相关/longest_common_prefix_14.py
@@ -6,21 +6,49 @@
 # @Software: PyCharm
 # @github  :https://github.com/Max-Liuhu
 
+
+# 利用zip
+def longest_common_prefix(strs):
+    if not strs:
+        return ''
+    res = ''
+    for tem in zip(*strs):
+        if len(set(tem)) == 1:
+            res += tem[0]
+        else:
+            break
+    return res
+
+
+# 按字典排序数组，比较第一个，和最后一个单词
+def longest_common_prefix_by_sort(strs):
+    if not strs:
+        return ''
+
+    strs.sort()
+    start = strs[0]
+    end = strs[-1]
+    for i, s in enumerate(start):
+        if start[i] == end[i]:
+            continue
+        else:
+            break
+    return start[:i]
+
+
 def longestCommonPrefix(strs):
     """
     :type strs: List[str]
     :rtype: str
     """
-    min_n=-1
+    min_n = -1
     for m in strs:
-        print min_n
         n = len(m)
         if min_n == -1:
             min_n = n
         if n < min_n:
             min_n = n
 
-    print min_n
     if not min_n:
         return ''
     mark = False
@@ -32,15 +60,9 @@ def longestCommonPrefix(strs):
             if num[j] != compare:
                 mark = True
                 break
-        print j
+
         if not mark:
             j += 1
-    print j
     if not first[0:j]:
         return ''
     return first[0:j]
-
-
-s = ["flower", "flow", "flight"]
-# print longestCommonPrefix(s)
-print longestCommonPrefix(["","b"])
