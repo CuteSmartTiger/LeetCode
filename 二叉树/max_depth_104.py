8b44ceaee007e53e4c14c650843976bcf1c2433c
invictus090 <ecustbaihu@163.com>
2020-04-28 23:30:18 +0800

    max_depth

--- 二叉树/max_depth_104.py
+++ 二叉树/max_depth_104.py
@@ -8,15 +8,31 @@
 # @github  :https://github.com/Max-Liuhu
 # Definition for a binary tree node.
 
-
-class TreeNode(object):
-    def __init__(self, x):
+class TreeNode:
+    def __init__(self, x, left_node=None, right_node=None):
         self.val = x
-        self.left = None
-        self.right = None
+        self.left = left_node
+        self.right = right_node
 
 
 class Solution:
+
+    def max_depth_queue(self, root):
+        if not root:
+            return 0
+        from queue import Queue
+        s = Queue()
+        s.put((1, root))
+        depth = 0
+        while not s.empty():
+            cur_depth, node = s.get()  # 这里一定要是先进先出，保证最后一个进入的最后出来
+            depth = cur_depth  # 标记深度
+            if node.left:
+                s.put((cur_depth + 1, node.left))
+            if node.right:
+                s.put((cur_depth + 1, node.right))
+        return depth
+
     def max_depth(self, root: TreeNode) -> int:
         if not root:
             return 0
@@ -35,3 +51,26 @@ class Solution:
             return 0
         # return max(self.max_depth(root.left) + 1, self.max_depth(root.right) + 1)
         return max(self.max_depth(root.left), self.max_depth(root.right)) + 1
+
+
+import unittest
+
+
+class TestRotateRight(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+        self.binary_tree = TreeNode(
+            3,
+            left_node=TreeNode(9),
+            right_node=TreeNode(
+                20,
+                left_node=TreeNode(15),
+                right_node=TreeNode(7)
+            )
+        )
+
+    def test_preorder_traversal_iter(self):
+        depth = self.s.max_depth_queue(self.binary_tree)
+        self.assertEqual(depth, 3)
+
+
