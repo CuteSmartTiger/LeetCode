3c61b82e0483d2295d170b77f11a60f81c5cc1ee
invictus090 <ecustbaihu@163.com>
2020-04-28 22:30:50 +0800

    is_symmetric

--- 二叉树/is_symmetric_101.py
+++ 二叉树/is_symmetric_101.py
@@ -15,30 +15,8 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if root is None:
-            return True
-        queue_list = [root.left, root.right]
-        while queue_list:
-            left = queue_list.pop(0)
-            right = queue_list.pop(0)
-            if not left and not right:
-                continue
-            if not left or not right:
-                return False
-            if left.val != right.val:
-                return False
-            queue_list.append(left.left)
-            queue_list.append(right.right)
-            queue_list.append(left.right)
-            queue_list.append(right.left)
-        return True
 
+class Solution(object):
     def is_symmetric_iter(self, root):
         if root is None:
             return True
@@ -59,20 +37,20 @@ class Solution(object):
             queue_list.append(right.left)
         return True
 
-
     def is_symmetric_recursion(self, root):
         if root is None:
             return True
         left_node = root.left
         right_node = root.right
 
-        def ismirror(left, right):
+        def is_mirror(left, right):
             if not left and not right:
                 return True
             if not left or not right:
                 return False
-            is_left_mirror = ismirror(left.left, right.right)
-            is_right_mirror = ismirror(left.right, right.left)
+            is_left_mirror = is_mirror(left.left, right.right)
+            is_right_mirror = is_mirror(left.right, right.left)
             return left.val == right.val and is_left_mirror and is_right_mirror
-        res = ismirror(left_node, right_node)
+
+        res = is_mirror(left_node, right_node)
         return res
