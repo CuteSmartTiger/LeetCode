db4cefe662d649cf1a4ca24137a650ecfbbd88b7
invictus090 <ecustbaihu@163.com>
2020-07-11 16:53:13 +0800

    复制复杂链表

--- 链表/copy_random_list_138.py
+++ 链表/copy_random_list_138.py
@@ -14,68 +14,56 @@ class Node:
         self.next = next
         self.random = random
 
+    def output(self):
+        visited = {}
+        output = []
+        append = output.append
+        cur = self
+        while cur:
+            if cur not in visited:
+                append((cur.val, cur.random.val if cur.random is not None else None))
+                cur = cur.next
+            else:
+                break
+        return output
 
-# 第一次时的解法,None是可哈希的
-# 第一步 存储对应的节点
-# 存储对应的next 与random节点
-def copy_random_list_1(head):
-    # 遍历存储节点与复制节点的映射关系
-    if not head:
-        return head
 
-    node_hash = {}
-    cur = head
-    while cur:
-        if cur not in node_hash:
-            node_hash[cur] = Node(cur.val)
+class Methods(object):
+    @classmethod
+    def methods(cls):
+        test_methods = []
+        append = test_methods.append
+        for attr in dir(cls):
+            if attr.startswith("__") or attr.endswith("__") or attr == 'methods':
+                continue
+            if callable(getattr(cls, attr)):
+                append(attr)
+        return test_methods
+
+
+class Solution(Methods):
+
+    def copy_random_list_by_hash(self, head):
+        # 遍历存储节点与复制节点的映射关系
+        if not head:
+            return head
+
+        node_hash = {}
+        cur = head
+        while cur:
+            if cur not in node_hash:
+                node_hash[cur] = Node(cur.val)
+                cur = cur.next
+            else:
+                break
+        cur = head
+        while cur:
+            node_hash[cur].next = node_hash.get(cur.next)
+            node_hash[cur].random = node_hash.get(cur.random)
             cur = cur.next
-        else:
-            break
-    cur = head
-    while cur:
-        node_hash[cur].next = node_hash.get(cur.next)
-        node_hash[cur].random = node_hash.get(cur.random)
-        cur = cur.next
 
-    return node_hash[head]
+        return node_hash[head]
 
-
-# 复制 然后拆分 O(1)复杂度
-def copy_random_list(head):
-    if not head:
-        return head
-
-    # 插入复制的节点
-    cur = head
-    while cur:
-        next_node = cur.next
-        cur.next = Node(cur.val)
-        cur.next.next = next_node
-        cur = next_node
-
-    # 往复制节点中关联random信息
-    cur = head
-    while cur:
-        # 注意random指向空节点时
-        if cur.random:
-            cur.next.random = cur.random.next
-        cur = cur.next.next
-
-    # 拆开，注意最后两的拆分，对空节点的处理
-    cur = head
-    copy_head = cur_copy = cur.next
-    while cur_copy.next:
-        cur.next = cur.next.next
-        cur = cur.next
-
-        cur_copy.next = cur_copy.next.next
-        cur_copy = cur_copy.next
-
-    cur.next = cur_copy.next
-    return copy_head
-
-
-class Solution:
     def copy_random_list(self, head):
         if not head:
             return head
@@ -103,3 +91,43 @@ class Solution:
                 copy_cur = copy_cur.next
 
         return copy_head
+
+
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node1.next = node2
+    node2.next = node3
+    node1.random = node3
+    node2.random = node2
+
+    node4 = None
+
+    node5 = Node(1)
+    node5.next = node5
+    # node5.random = node5
+
+    def setUp(self):
+        self.s = Solution()
+        self.nodes_input_to_output = [
+            (self.node1, [(1, 3), (2, 2), (3, None)]),
+            (self.node4, Node),
+            # (self.node5, [(1, 1)]),
+            (self.node5, [(1, 1)]),
+        ]
+
+    def test_methods(self):
+        from itertools import product
+        from copy import deepcopy
+        for method, (node, output) in product(self.s.methods(), self.nodes_input_to_output):
+            test_node = deepcopy(node)
+            n = getattr(self.s, method)(test_node)
+            if n:
+                self.assertEqual(n.output(), output)
+                print(n.output(), output)
+            else:
+                print(n)
